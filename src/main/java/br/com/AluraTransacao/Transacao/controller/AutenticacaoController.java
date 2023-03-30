950c2669ea27afd539ec52ec010d207e9585f73a
DavidPaulinus <davipm@hotmail.com>
2023-03-30 10:58:38 -0300

    Gerador de tokens JWT

--- src/main/java/br/com/AluraTransacao/Transacao/controller/AutenticacaoController.java
+++ src/main/java/br/com/AluraTransacao/Transacao/controller/AutenticacaoController.java
@@ -1,6 +1,7 @@
 package br.com.AluraTransacao.Transacao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.AluraTransacao.Transacao.infra.TokenService;
+import br.com.AluraTransacao.Transacao.model.Usuario;
 import br.com.AluraTransacao.Transacao.model.DTO.autenticacao.DadosAutenticacaoDTO;
 import jakarta.validation.Valid;
 
@@ -16,12 +19,14 @@ import jakarta.validation.Valid;
 public class AutenticacaoController {
 	@Autowired
 	private AuthenticationManager manager;
+	@Autowired
+	private TokenService tolkien;
 	
 	@PostMapping
-	public String efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dto){
+	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dto){
 		var token = new UsernamePasswordAuthenticationToken(dto.login(), dto.senha());
 		var authentication = manager.authenticate(token);
 		
-		return "redirect:/transacao";
+		return ResponseEntity.ok(tolkien.gerarToken((Usuario)authentication.getPrincipal()));
 	}
 }
